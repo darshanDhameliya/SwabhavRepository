18a7286d70eb44187ba8d80ec2261b2bda30bcac
darshanDhameliya <darshandhameliya18@gmail.com>
2019-07-18 12:44:20 +0530

    complete multiBind with ngModel change event , change text ,text size ,text bacgrondColor,text fontColor

--- nodeJs/angular7/welcome-app/src/app/app.module.ts
+++ nodeJs/angular7/welcome-app/src/app/app.module.ts
@@ -4,18 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 import { StudentComponent } from './student/student.component';
+import { MultiBind } from './multiBind/multiBind.component'; 
 import { CgpaBedge } from './pipes/cgpaBedge.pipe';
 
 @NgModule({
   declarations: [
     WelcomeComponent,
     StudentComponent,
+    MultiBind,
     CgpaBedge
   ],
   imports: [
     BrowserModule,FormsModule
   ],
   providers: [],
-  bootstrap: [WelcomeComponent,StudentComponent]
+  bootstrap: [WelcomeComponent,StudentComponent,MultiBind]
 })
 export class AppModule { }
---
+++ nodeJs/angular7/welcome-app/src/app/multiBind/multiBind.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'swabhav-multiBind',
+    templateUrl:'./multiBind.component.html'
+})
+
+export class MultiBind{
+}
