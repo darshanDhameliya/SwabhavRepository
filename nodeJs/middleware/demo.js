6c7c6cc6b7d8660093d1ca498da61a7aeb9eb1b4
darshanDhameliya <darshandhameliya18@gmail.com>
2019-08-01 17:16:13 +0530

    complate middleware demo in node js

--- nodeJs/middleware/demo.js
+++ nodeJs/middleware/demo.js
@@ -1,13 +1,30 @@
-var connect = require('connect');
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var app = connect()
-app.use('/foo', function fooMiddleware(req, res, next) {
-    console.log('foo');
+
+app.use(express.static('public'));
+
+function compressionMiddleware(req, res, next) {
+    console.log('inside compression middleware');
     next();
-});
-app.use('/bar', function barMiddleware(req, res, next) {
-    console.log('bar');
+}
+
+function encryptionMiddleware(req, res, next) {
+    console.log('inside encryption middleware');
     next();
+}
+
+function analysisMiddleware(req, res, next) {
+    console.log('inside analysis middleware');
+    next();
+}
+app.use('/login', compressionMiddleware, encryptionMiddleware, analysisMiddleware);
+
+app.get('/login', function(req, res) {
+    console.log('inside login controller');
+    res.end();
 });
-http.createServer(app).listen(3000);
+
+app.listen(3000, () => {
+    console.log('server running');
+});
