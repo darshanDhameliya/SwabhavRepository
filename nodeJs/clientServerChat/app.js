2fa406911bc2b7265882e5ce92239ed1be3e9cdd
darshanDhameliya <darshandhameliya18@gmail.com>
2019-08-08 17:26:12 +0530

    comlate socket io -connection start and close - total connection count and private chat between server and client

--- nodeJs/clientServerChat/app.js
+++ nodeJs/clientServerChat/app.js
@@ -2,18 +2,35 @@ let Express = require('express');
 let app = Express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let PORT = 8080;
+let clientMap = new Map();
 
 app.use(Express.static('public'));
 
 io.on('connection', (socket) => {
+
+    console.log('------------------------------------------------------------');
     console.log('A new websocket connection has been established');
+    console.log('connected : ' + socket.id);
+    clientMap.set(socket.id, '');
+    console.log('total connection : ' + clientMap.size);
 
-    socket.on('chat message', function(msg) {
-        console.log('client : ' + msg);
 
-        io.emit('chat message', 'hello client');
+
+    socket.on('chat message', function(data) {
+        clientMap.set(socket.id, data);
+        console.log('------------------------------------------------------------');
+        console.log(data.SOCKETID + '{\n\tuserName : ' + data.USERNAME + ',\n\tmessage : ' + data.MESSAGE + '\n}');
+        io.to(data.SOCKETID).emit('chat message', 'hello client');
+    });
+
+    socket.on('disconnect', () => {
+        clientMap.delete(socket.id);
+        console.log('------------------------------------------------------------');
+        console.log('disconnected : ' + socket.id);
+        console.log('total connection : ' + clientMap.size);
     });
 });
-http.listen(8080, () => {
-    console.log('server connected  localhost:8080');
+http.listen(PORT, () => {
+    console.log('server connected  localhost:' + PORT);
 });
